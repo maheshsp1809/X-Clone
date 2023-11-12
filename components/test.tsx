cd5025799669faf3124191a22dd3391cc9e92353
maheshsp <maheshsp1809@gmail.com>
2023-11-12 23:05:57 +0530

    testing

---
+++ components/test.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <div>
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+export default Counter;
